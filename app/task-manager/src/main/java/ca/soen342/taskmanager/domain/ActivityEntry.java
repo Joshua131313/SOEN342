304d47f9273ac93cc18e836840cd1bef1518b996
Joshua Bitton <joshtosh13@hotmail.com>
2026-03-02 17:37:08 -0500

    Added remaining domain classes
    Added collaborator, project and category classes.
    Implemented SubTask class and ActivityEntry class

--- app/task-manager/src/main/java/ca/soen342/taskmanager/domain/ActivityEntry.java
+++ app/task-manager/src/main/java/ca/soen342/taskmanager/domain/ActivityEntry.java
@@ -1,5 +1,30 @@
 package ca.soen342.taskmanager.domain;
 
+import java.time.LocalDate;
+
 public class ActivityEntry {
+    private LocalDate timestamp;
+    private String description;
+    
+    public ActivityEntry(LocalDate timestamp, String description) {
+        this.timestamp = timestamp;
+        this.description = description;
+    }
+
+    public LocalDate getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDate timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
     
 }
--- app/task-manager/src/main/java/ca/soen342/taskmanager/domain/SubTask.java
+++ app/task-manager/src/main/java/ca/soen342/taskmanager/domain/SubTask.java
@@ -1,5 +1,30 @@
 package ca.soen342.taskmanager.domain;
 
+import ca.soen342.taskmanager.enums.Status;
+
 public class SubTask {
+    private String title;
+    private Status status;
+
+    public SubTask(String title, Status status) {
+        this.title = title;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
     
 }
